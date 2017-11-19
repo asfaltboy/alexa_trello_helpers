8f6e436b8ae0c443496395f721e5329a5aadb4f7
Pavel Savchenko <asfaltboy@gmail.com>
2017-11-19 09:52:01 +0100

    Initial commit

---
+++ shopping_skill.py
@@ -0,0 +1,32 @@
+from os.path import join, dirname
+from os import environ
+
+from dotenv import load_dotenv
+import alexandra
+
+alex = alexandra.Application()
+token_map = {}
+
+
+@alex.intent("ListBoards")
+def list_boards_intent(slots, session):
+    assert session.user_id in token_map, (
+        'Session.user_id "%s" was not found in token map!' % session.user_id)
+    return alex.respond("Listing your boards")
+
+
+def setup_tokens():
+    """ Parse env vars for user Trello board tokens """
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path, verbose=True)
+
+    api_key = environ.get('TRELLO_API_KEY')
+    assert api_key, 'Missing TRELLO_API_KEY value!'
+    for key, value in environ.items():
+        if key.startswith('TRELLO_API_TOKEN_'):
+            token_map[key[6:]] = value
+
+
+if __name__ == '__main__':
+    setup_tokens()
+    alex.run('127.0.0.1', 8080, debug=True)
