e06593b444d5045df64e30d981088d8e53f34112
Pavel Savchenko <asfaltboy@gmail.com>
2017-11-19 21:01:40 +0100

    Move more trello functionality to trello_utils

--- trello_intents.py
+++ trello_intents.py
@@ -1,16 +1,9 @@
-from os.path import join, dirname
-from os import environ
-
-from dotenv import load_dotenv
 import alexandra
 
-from trello_utils import get_client
+from trello_utils import trello_client
 
 alex = alexandra.Application()
 wsgi = alex.create_wsgi_app()
-
-# TODO: use a persistant store instead of in-memory
-user_token_map = {}
 
 
 @alex.intent("ListBoards")
@@ -22,17 +15,13 @@ def list_boards_intent(slots, session):
 
 @alex.intent("SetDefaultBoard")
 def set_default_board_intent(slots, session):
+    board = slots['Board']
     client = trello_client(session)
-    board = ', '.join(l.name for l in client.list_boards())
+    boards = [l.name for l in client.list_boards()]
+    if not board or board not in boards:
+        return alexandra.respond(f"Invalid board {board}")
+    # TODO: persist change to DB
     return alexandra.respond(f"Set the board {board} as default")
-
-
-@alex.intent("ListCardsInBoard")
-def list_cards_in_board_intent(slots, session):
-    client = trello_client(session)
-    boards = ', '.join(l.name for l in client.list_boards())
-    return alexandra.respond(f"Listing your boards: {boards}")
-
 
 
 @alex.intent("ListsInBoard")
@@ -42,28 +31,12 @@ def lists_in_board_intent(slots, session):
     return alexandra.respond(f"Listing your boards: {boards}")
 
 
-def trello_client(session):
-    global user_token_map
-    if not user_token_map:
-        api_key, user_token_map = setup_tokens()
-    assert session.user_id in user_token_map, (
-        'Session.user_id "%s" was not found in token map!' % session.user_id)
-    return get_client(api_key=api_key, token=user_token_map[session.user_id])
-
-
-def setup_tokens():
-    """ Parse env vars for user Trello board tokens """
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path, verbose=True)
-
-    api_key = environ.get('TRELLO_API_KEY')
-    assert api_key, 'Missing TRELLO_API_KEY value!'
-    for key, value in environ.items():
-        if key.startswith('TRELLO_API_TOKEN_'):
-            user_token_map[key[17:]] = value
-    return api_key, user_token_map
+@alex.intent("ListCardsInBoard")
+def list_cards_in_board_intent(slots, session):
+    client = trello_client(session)
+    boards = ', '.join(l.name for l in client.list_boards())
+    return alexandra.respond(f"Listing your boards: {boards}")
 
 
 if __name__ == '__main__':
-    setup_tokens()
     alex.run('127.0.0.1', 8080, debug=True)
--- trello_utils.py
+++ trello_utils.py
@@ -1,6 +1,13 @@
+from os.path import join, dirname
+from os import environ
+
+from dotenv import load_dotenv
 from trello import TrelloClient
 
 _client = None
+
+# TODO: use a persistant store instead of in-memory
+user_token_map = {}
 
 
 def get_client(api_key, token, api_secret=None, token_secret=None):
@@ -17,3 +24,25 @@ def get_client(api_key, token, api_secret=None, token_secret=None):
             token_secret=token_secret
         )
     return _client
+
+
+def trello_client(session):
+    global user_token_map
+    if not user_token_map:
+        api_key, user_token_map = setup_tokens()
+    assert session.user_id in user_token_map, (
+        'Session.user_id "%s" was not found in token map!' % session.user_id)
+    return get_client(api_key=api_key, token=user_token_map[session.user_id])
+
+
+def setup_tokens():
+    """ Parse env vars for user Trello board tokens """
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path, verbose=True)
+
+    api_key = environ.get('TRELLO_API_KEY')
+    assert api_key, 'Missing TRELLO_API_KEY value!'
+    for key, value in environ.items():
+        if key.startswith('TRELLO_API_TOKEN_'):
+            user_token_map[key[17:]] = value
+    return api_key, user_token_map
